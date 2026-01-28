e71e16e3a566c3893a46b7103d73e550f7bc2d15
Ankit Kumar <ankitkr5447@gmail.com>
2026-01-28 16:42:53 +0530

    Updates

--- NoteItem.js
+++ NoteItem.js
@@ -1,48 +1,14 @@
 class NoteItem {
- 
   title;
   desc;
   due_at;
-  created_at;
+  created_at = new Date().toLocaleDateString("en-CA");;
+  id = Number(localStorage.getItem("currentId"));
+  isDone = false;
   constructor(title, desc, dueAt) {
     this.title = title;
     this.desc = desc;
-    this.created_at = new Date().toLocaleDateString("en-CA");
-    this.due_at =  dueAt;
-  }
-
- 
-
-  getTitle() {
-    return this.title;
-  }
-
-  setTitle(title) {
-    this.title = title;
-  }
-
-  getDesc() {
-    return this.desc;
-  }
-
-  setDesc(desc) {
-    this.desc = desc;
-  }
-
-  getDueAt() {
-    return this.due_at;
-  }
-
-  setDueAt(dueAt) {
     this.due_at = dueAt;
   }
-
-  getCreatedAt() {
-    return this.created_at;
-  }
-
-  setCreatedAt(createdAt) {
-    this.created_at = createdAt;
-  }
 }
-export default NoteItem
+export default NoteItem;
